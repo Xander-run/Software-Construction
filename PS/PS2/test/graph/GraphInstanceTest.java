51ca342b62bf0139458709ae5d09c929593edd3c
unknown <fluent_python@qq.com>
2022-01-18 17:01:17 +0800

    finish GraphInstancetest

--- PS/PS2/test/graph/GraphInstanceTest.java
+++ PS/PS2/test/graph/GraphInstanceTest.java
@@ -19,9 +19,14 @@ import org.junit.Test;
  */
 public abstract class GraphInstanceTest {
     
-    // Testing strategy
-    //   TODO
-    
+    /* Testing Strategy:
+     * FIXME: I don't think this is necessary. The test is not a partition but a sequence of operation rather.
+     *  also, the code will illustrate the sequence, and no need to write redundant comments.
+     */
+
+    private static final String string1 = "aaa";
+    private static final String string2 = "bbb";
+
     /**
      * Overridden by implementation-specific test classes.
      * 
@@ -40,7 +45,82 @@ public abstract class GraphInstanceTest {
         assertEquals("expected new graph to have no vertices",
                 Collections.emptySet(), emptyInstance().vertices());
     }
-    
-    // TODO other tests for instance methods of Graph
-    
+
+    @Test
+    public void testAdd() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add(string1));
+        assertFalse(graph.add(string1));
+        assertTrue(graph.add(string2));
+        assertFalse(graph.add(string2));
+    }
+
+    @Test
+    public void testRemove() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add(string1));
+        assertTrue(graph.add(string2));
+        assertEquals(0, graph.set(string1, string2, 1));
+        assertEquals(1, graph.sources(string2).size());
+        assertTrue(graph.remove(string1));
+        assertEquals(0, graph.sources(string2).size());
+        assertTrue(graph.remove(string2));
+        assertFalse(graph.remove(string1));
+        assertFalse(graph.remove(string2));
+    }
+
+    @Test
+    public void testSet() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add(string1));
+        assertTrue(graph.add(string2));
+        assertEquals(0, graph.set(string1, string2, 1));
+        assertEquals(1, graph.set(string1, string2, 2));
+        assertEquals(0, graph.set(string2, string1, 3));
+        assertEquals(3, graph.set(string2, string1, 4));
+    }
+
+    @Test
+    public void testVertices() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add(string1));
+        assertTrue(graph.add(string2));
+        assertEquals(2, graph.vertices().size());
+        assertTrue(graph.vertices().contains(string1));
+        assertTrue(graph.vertices().contains(string2));
+        assertTrue(graph.remove(string1));
+        assertEquals(1, graph.vertices().size());
+        assertTrue(graph.vertices().contains(string1));
+        assertFalse(graph.vertices().contains(string2));
+    }
+
+    @Test
+    public void testSources() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add(string1));
+        assertTrue(graph.add(string2));
+        assertEquals(0, graph.sources(string1).keySet().size());
+        assertEquals(0, graph.sources(string2).keySet().size());
+        assertEquals(0, graph.set(string1, string2, 3));
+        assertEquals(0, graph.set(string2, string1, 4));
+        assertEquals(1, graph.sources(string1).keySet().size());
+        assertEquals(1, graph.sources(string2).keySet().size());
+        assertEquals(Integer.valueOf(3), graph.sources(string2).get(string1));
+        assertEquals(Integer.valueOf(4), graph.sources(string1).get(string2));
+    }
+
+    @Test
+    public void testTargets() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add(string1));
+        assertTrue(graph.add(string2));
+        assertEquals(0, graph.targets(string1).keySet().size());
+        assertEquals(0, graph.targets(string2).keySet().size());
+        assertEquals(0, graph.set(string1, string2, 3));
+        assertEquals(0, graph.set(string2, string1, 4));
+        assertEquals(1, graph.targets(string1).keySet().size());
+        assertEquals(1, graph.targets(string2).keySet().size());
+        assertEquals(Integer.valueOf(3), graph.targets(string1).get(string2));
+        assertEquals(Integer.valueOf(4), graph.targets(string2).get(string1));
+    }
 }
