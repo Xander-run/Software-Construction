28f12c1776ca270146a6aa1607b65692b7f4ab67
unknown <fluent_python@qq.com>
2022-01-22 20:55:45 +0800

    Initialize PS3

---
+++ PS/PS3/src/expressivo/Expression.java
@@ -0,0 +1,57 @@
+/* Copyright (c) 2015-2016 MIT 6.005 course staff, all rights reserved.
+ * Redistribution of original or derived work requires permission of course staff.
+ */
+package expressivo;
+
+/**
+ * An immutable data type representing a polynomial expression of:
+ *   + and *
+ *   nonnegative integers and floating-point numbers
+ *   variables (case-sensitive nonempty strings of letters)
+ * 
+ * <p>PS3 instructions: this is a required ADT interface.
+ * You MUST NOT change its name or package or the names or type signatures of existing methods.
+ * You may, however, add additional methods, or strengthen the specs of existing methods.
+ * Declare concrete variants of Expression in their own Java source files.
+ */
+public interface Expression {
+    
+    // Datatype definition
+    //   TODO
+    
+    /**
+     * Parse an expression.
+     * @param input expression to parse, as defined in the PS3 handout.
+     * @return expression AST for the input
+     * @throws IllegalArgumentException if the expression is invalid
+     */
+    public static Expression parse(String input) {
+        throw new RuntimeException("unimplemented");
+    }
+    
+    /**
+     * @return a parsable representation of this expression, such that
+     * for all e:Expression, e.equals(Expression.parse(e.toString())).
+     */
+    @Override 
+    public String toString();
+
+    /**
+     * @param thatObject any object
+     * @return true if and only if this and thatObject are structurally-equal
+     * Expressions, as defined in the PS3 handout.
+     */
+    @Override
+    public boolean equals(Object thatObject);
+    
+    /**
+     * @return hash code value consistent with the equals() definition of structural
+     * equality, such that for all e1,e2:Expression,
+     *     e1.equals(e2) implies e1.hashCode() == e2.hashCode()
+     */
+    @Override
+    public int hashCode();
+    
+    // TODO more instance methods
+    
+}
---
+++ PS/PS3/test/expressivo/ExpressionTest.java
@@ -0,0 +1,26 @@
+/* Copyright (c) 2015-2016 MIT 6.005 course staff, all rights reserved.
+ * Redistribution of original or derived work requires permission of course staff.
+ */
+package expressivo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Tests for the Expression abstract data type.
+ */
+public class ExpressionTest {
+
+    // Testing strategy
+    //   TODO
+    
+    @Test(expected=AssertionError.class)
+    public void testAssertionsEnabled() {
+        assert false; // make sure assertions are enabled with VM argument: -ea
+    }
+    
+    
+    // TODO tests for Expression
+    
+}
